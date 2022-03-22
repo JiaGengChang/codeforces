9d0c0227a2162ea8e7e53ecab3f59516210c4f29
jiagengchang <jiageng1997@gmail.com>
2022-03-22 22:26:48 +0800

    Shortened

--- .vim/templates/sol.cpp
+++ .vim/templates/sol.cpp
@@ -1,23 +1,9 @@
-#include <cmath>
-#include <climits>
-#include <queue>
-#include <vector>
-#include <map>
-#include <cstdlib>
-#include <fstream>
-#include <iomanip>   
-#include <iostream>  
-#include <sstream>
-#include <stack>
-#include <algorithm>
-#include <numeric>
-#include <cstring>
-#include <cassert>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 #ifndef ONLINE_JUDGE
-#include "debug.h"
+#include "../../debug.h"
 #else 
 #define debug(x...)
 #endif
@@ -29,23 +15,20 @@ using namespace std;
 #define ALL(c) (c).begin(),(c).end() 
 #define RALL(c) (c).rbegin(),(c).rend() 
 #define PRESENT(c,x) ((c).find(x) != (c).end())
-#define ll long long 
-#define ull unsigned long long
-#define ui unsigned int
-#define IOS ios_base::sync_with_stdio(0); 
-#define INF 1001001001
+#define IOS ios_base::sync_with_stdio(0)
 #define MOD 1000000007
 #define PI 3.1415926535897932384626
 #define MX 100001
 #define printA(a,L,R) F(i,L,R) cout << a[i] << (i==R-1?'\n':' ')
-#define printV(a) printA(a,0,a.size())
 #define f first
 #define s second
 #define ins insert
 #define lb lower_bound
 #define ub upper_bound
-#define mp make_pair
 #define pb push_back
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int ui;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef pair<int,int> ii;
@@ -55,19 +38,17 @@ typedef pair<ll,ll> llll;
 template<class T> bool ckmin(T& a,const T& b) {return b<a?a=b,1:0;}
 template<class T> bool ckmax(T& a,const T& b) {return b>a?a=b,1:0;}
 
-void solve() {
-    ;
-}
-
-signed main() {
+int main() {
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     freopen("error.txt","w", stderr);
 #endif
+    IOS;
     cin.tie(0); 
     cout.tie(0);
-    solve();
+    //solve
+    ;
     return 0;
 }
 
