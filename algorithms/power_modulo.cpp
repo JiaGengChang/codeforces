f66f3707e1e17c46ed801a6f7775ba47a96c0d55
jiagengchang <jiageng1997@gmail.com>
2022-03-25 06:45:36 +0800

    shortened power mod snippet

--- algorithms/power_modulo.cpp
+++ algorithms/power_modulo.cpp
@@ -1,15 +1,11 @@
-
-ll mpow(ll b, ll e) {
+ll mod_pow(ll b, ll e) {
     ll x=1;
     while(e>0){
-        if(e%2==1){
-            x = (x*b)%mod;
-            --e;
+        if(e&1){
+            x = (x*b)%mod; --e;
         } else {
-            b = (b*b)%mod;
-            e /= 2;
+            b = (b*b)%mod; e/=2;
         }
     }
     return x;
 }
-
