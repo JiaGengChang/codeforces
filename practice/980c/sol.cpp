ff01fb3b39b69c16a92caf380c97da28f788ae93
jiagengchang <jiageng1997@gmail.com>
2022-03-25 01:06:02 +0800

    980c

---
+++ practice/980c/sol.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#include "../../debug.h"
+#else 
+#define debug(x...)
+#endif
+
+using namespace std;
+
+#define sz(x) ((int)((x).size()))
+#define F(i,L,R) for (int i = L; i < R; i++) 
+#define FE(i,L,R) for (int i = L; i <= R; i++)
+#define FF(i,L,R) for (int i = L; i > R; i--)
+#define FFE(i,L,R) for (int i = L; i >= R; i--)
+#define all(c) (c).begin(),(c).end() 
+#define rall(c) (c).rbegin(),(c).rend() 
+#define present(c,x) ((c).find(x) != (c).end())
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define prints(a,L,R) F(i,L,R) cout << a[i] << (i==R-1?'\n':' ')
+#define f first
+#define s second
+#define ins insert
+#define fif find_if
+#define bkin back_inserter
+#define cpif copy_if
+#define mxel max_element
+#define mnel min_element
+#define lb lower_bound
+#define ub upper_bound
+#define pb push_back
+#define inf 1001001001
+#define mod 1000000007
+#define pi 3.1415926535897932384626
+#define mx 100001
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int ui;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int,int> ii;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef pair<ll,ll> llll;
+template<class T> bool ckmin(T& a,const T& b) {return b<a?a=b,1:0;}
+template<class T> bool ckmax(T& a,const T& b) {return b>a?a=b,1:0;}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w", stderr);
+#endif
+    fastio;
+    int n,k;
+    cin>>n>>k;
+    vi p(n), key(256,-1);
+    key[0]=0;
+    F(i,0,n) cin>>p[i];
+    F(i,0,n) {
+        if (key[p[i]]>=0) continue;
+        if (p[i]==0) continue;
+        int pj=p[i];
+        while (pj>0){
+            if (key[pj]>=0 || p[i]-pj == k-1) break; else --pj;
+        }
+        if (key[pj]>=0){
+            FE(ptmp,pj+1,p[i]) key[ptmp]=key[pj];
+        } else {
+            FE(ptmp,pj,p[i]) key[ptmp]=pj;
+        }
+    }
+    F(i,0,n) cout<<key[p[i]]<<" ";
+    return 0;
+}
+
+
