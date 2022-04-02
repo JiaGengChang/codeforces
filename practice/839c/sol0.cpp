b5b0a87767c99c46c714921102e4ed764078673c
jiagengchang <jiageng1997@gmail.com>
2022-04-02 12:39:43 +0800

    839c journey: graphs, expected distance

---
+++ practice/839c/sol0.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
+#include "../../debug.h"
+#else 
+#define debug(x...)
+#endif
+
+using namespace std;
+
+#define sz(x) ((int)((x).size()))
+#define mt(x) ((x).empty())
+#define FI(it,x) for (auto it=(x).begin(); it!=(x).end(); ++it)
+#define F(i,L,R) for (int i = L; i < R; i++) 
+#define FE(i,L,R) for (int i = L; i <= R; i++)
+#define FF(i,L,R) for (int i = L; i > R; i--)
+#define FFE(i,L,R) for (int i = L; i >= R; i--)
+#define all(c) (c).begin(),(c).end() 
+#define rall(c) (c).rbegin(),(c).rend() 
+#define present(c,x) ((c).find(x) != (c).end())
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define prints(a,L,R) F(i,L,R) cout << a[i] << (i==R-1?'\n':' ')
+#define f first
+#define s second
+#define ins insert
+#define fif find_if
+#define bkin back_inserter
+#define cpif copy_if
+#define mxel max_element
+#define mnel min_element
+#define lb lower_bound
+#define ub upper_bound
+#define pb push_back
+#define ppb pop_back
+#define inf 1001001001
+#define mod 1000000007
+#define pi 3.1415926535897932384626
+#define mx 100001
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int ui;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int,int> ii;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+template<class T> bool ckmin(T& a,const T& b) {return b<a?a=b,1:0;}
+template<class T> bool ckmax(T& a,const T& b) {return b>a?a=b,1:0;}
+template<class T> bool ckval(T& a,const T& b) {return a<0?a=b,1:0;}
+
+vector<bool> vis;
+vvi g;
+
+double dfs(int v,int d,double pv){
+    //debug(v,d,pv);
+    double ans=0;
+    if(!vis[v]){
+        vis[v]=true;
+        if (sz(g[v])==1 && vis[g[v][0]]){
+            ans = pv*(double)d;
+        } else {
+            int deg=0;
+            for(int u:g[v]) if(!vis[u]) ++deg;
+            for(int u:g[v]) 
+                ans += dfs(u, d+1, pv/(double)deg);
+        }
+    }
+    return ans;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w", stderr);
+#endif
+    fastio;
+    int n; cin>>n;
+    g.resize(n); 
+    vis.resize(n,false);
+    F(i,0,n-1) {
+        int u,v;
+        cin>>u>>v; --u,--v;
+        g[u].pb(v);
+        g[v].pb(u);
+    }
+    //debug(g);
+    cout<<setprecision(10);
+    cout<<dfs(0,0,1.0);
+    return 0;
+}
+
+
