2a75fb8d91534e101c96e374f1eab01efa3c04d8
jiagengchang <jiageng1997@gmail.com>
2022-04-06 16:10:37 +0800

    1200c math, number th

---
+++ practice/1200c/sol.cpp
@@ -0,0 +1,74 @@
+#include "bits/stdc++.h"
+#ifndef ONLINE_JUDGE
+#include "../../debug.h"
+#else 
+#define debug(x...)
+#endif
+
+using namespace std;
+
+#define sz(x) ((int)((x).size()))
+#define mt(x) ((x).empty())
+#define FI(it,x) for (auto it=(x).begin(); it!=(x).end(); ++it)
+#define F(i,L,R) for (int i = L; i < R; i++) 
+#define FE(i,L,R) for (int i = L; i <= R; i++)
+#define FF(i,L,R) for (int i = L; i > R; i--)
+#define FFE(i,L,R) for (int i = L; i >= R; i--)
+#define all(c) (c).begin(),(c).end() 
+#define rall(c) (c).rbegin(),(c).rend() 
+#define present(c,x) ((c).find(x) != (c).end())
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define prints(a,L,R) F(i,L,R) cout << a[i] << (i==R-1?'\n':' ')
+#define f first
+#define s second
+#define ins insert
+#define fif find_if
+#define bkin back_inserter
+#define cpif copy_if
+#define mxel max_element
+#define mnel min_element
+#define lb lower_bound
+#define ub upper_bound
+#define pb push_back
+#define ppb pop_back
+#define inf 1001001001
+#define mod 1000000007
+#define pi 3.1415926535897932384626
+#define mx 100001
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int ui;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef set<int> si;
+typedef pair<int,int> ii;
+typedef set<pair<int,int>> sii;
+typedef vector<pair<int,int>> vii;
+typedef vector<bool> vb;
+template<class T> bool ckmin(T& a,const T& b) {return b<a?a=b,1:0;}
+template<class T> bool ckmax(T& a,const T& b) {return b>a?a=b,1:0;}
+template<class T> bool ckval(T& a,const T& b) {return a<0?a=b,1:0;}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt","w", stderr);
+#endif
+    fastio;
+    ll n,m;
+    int q;
+    cin>>n>>m>>q;
+    ll gcd = __gcd(n,m);
+    ll f[3]; f[1] = n/gcd, f[2] = m/gcd;
+    while (q--){
+        ll sx,sy,ex,ey;
+        cin>>sx>>sy>>ex>>ey;
+        ll s_sector = (sy-1)/f[sx];
+        ll e_sector = (ey-1)/f[ex];
+        cout << ((s_sector==e_sector) ? "YES\n" : "NO\n");
+    }
+    return 0;
+}
+
+ 
